9e2d442e61f1a1d1b89752afa1fe7193916dfa9e
TimmyKham <tkhamsithideth@gmail.com>
2019-06-07 15:08:02 +0200

    Last commit test integration

--- test/test.js
+++ test/test.js
@@ -6,61 +6,6 @@ const expect = chai.expect;
 
 chai.use(chaiHttp)
 
-describe('colors', () => {
-  beforeEach((done) => {
-    app.remove({}, () => {
-      done();
-    });
-  });
-  describe('/GET colors', () => {
-    it('​should return all colors', (done) => {
-      chai.request('http://localhost:8080').get('/colors').end((err, res) => {
-        expect(res).to.have.status(200);
-        res.should.have.status(200);
-        res.should.be.json;
-        res.body.should.be.a('array');
-        res.body.should.be.a('object');
-        done();
-      });
-    });
-  });
-  describe('/GET color', () => {
-    it('should return Bad Request​', (done) => {
-      chai.request('http://localhost:8080').get('/color').end((err, res) => {
-        expect(res).to.have.status(404);
-        done();
-      });
-    });
-  });
-  describe('/POST colors', () => {
-    it('should add new color​', (done) => {
-      const param = {
-        results: 'YELLOW'
-      };
-      chai.request('http://localhost:8080').post('/colors').send(param).end((err, res) => {
-        expect(res).to.have.status(201);
-        res.should.be.json;
-        res.body.should.be.a('object');
-        res.body.should.be.a('array');
-        res.body.name.should.eql(param.results);
-        done();
-      });
-    });
-  });
-  describe('/GET colors', () => {
-    it('​​should return new color list Request', (done) => {
-      chai.request('http://localhost:8080').get('/colors').end((err, res) => {
-        expect(res).to.have.status(200);
-        res.should.have.status(200);
-        res.should.be.json;
-        res.body.should.be.a('array');
-        res.body.should.be.a('object');
-        done();
-      });
-    });
-  });
-});
-
 const newValues = []
 const colorKey = 'NEW_COLOR_'
 let nextCursor = 0;
@@ -73,4 +18,54 @@ const payloadColor = () => {
 const getCurrentCulor = () => {
   return nextCursor > 0 ? `${colorKey}${nextCursor - 1}` : `${colorKey}O`
 }
-// <-- FIN
+
+describe('colors', () => {
+  describe('/GET colors', () => {
+    it('​should return all colors', (done) => {
+      chai.request(app).get('/colors').end((err, res) => {
+        expect(res).to.have.status(200);
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.results.should.be.a('array');
+        res.body.results.should.include.members(["RED","GREEN","BLUE"]);
+        done();
+      });
+    });
+  });
+  describe('/GET color', () => {
+    it('should return Bad Request​', (done) => {
+      chai.request(app).get('/color').end((err, res) => {
+        expect(res).to.have.status(404);
+        done();
+      });
+    });
+  });
+  describe('/POST colors', () => {
+    it('should add new color​', (done) => {
+      const param = payloadColor();
+      chai.request(app).post('/colors').send(param).end((err, res) => {
+        expect(res).to.have.status(201);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.results.should.be.a('array');
+        res.body.results.should.include.members([getCurrentColor()]);
+        done();
+      });
+    });
+  });
+  describe('/GET colors', () => {
+    it('​​should return new color list Request', (done) => {
+      chai.request(app).get('/colors').end((err, res) => {
+        expect(res).to.have.status(200);
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.results.should.be.a('array');
+        res.body.results.should.include.members("RED","GREEN","BLUE",getCurrentColor());
+        done();
+      });
+    });
+  });
+});
+

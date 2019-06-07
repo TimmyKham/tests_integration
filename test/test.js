35bdfc00b71622addad3e67ca56f8a097d5fe531
Geoffrey <geoffrey.beauny@publicissapient.com>
2019-06-07 12:18:34 +0200

    Update payload and current color function

--- test/test.js
+++ test/test.js
@@ -1,8 +1,26 @@
-'use strict';
+const chai = require('chai')
+const chaiHttp = require('chai-http')
+const app = require('../index')
+const should = chai.should()
+const expect = chai.expect
 
-const chai = require('chai');
-const expect = require('chai').expect;
+chai.use(chaiHttp)
 
-chai.use(require('chai-http'));
-
-const app = require('../index.js'); // Our app
+// ---> DEBUT
+/**
+  * Génération des nouvelles couleurs et enregistrement de ces
+  * couleurs dans un tableau.
+  */
+const newValues = []
+const colorKey = 'NEW_COLOR_'
+let nextCursor = 0;
+const payloadColor = () => {
+  const nextColor = `${colorKey}${nextCursor}`
+  newValues.push(nextColor)
+  nextCursor++;
+  return { 'color': nextColor }
+}
+const getCurrentCulor = () => {
+  return nextCursor > 0 ? `${colorKey}${nextCursor - 1}` : `${colorKey}O`
+}
+// <-- FIN
